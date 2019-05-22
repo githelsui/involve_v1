054b931474d73b5f9333a66d837c88de394eff9e
GITHELLYN SUICO <800196@district.pylusd.k12.ca.us>
2019-05-22 11:39:40 -0700

    new files

--- app/src/main/java/com/example/android/recyclerviewproject/ExampleItem.java
+++ app/src/main/java/com/example/android/recyclerviewproject/ExampleItem.java
@@ -1,4 +1,25 @@
 package com.example.android.recyclerviewproject;
 
 public class ExampleItem {
+    private int imageValue;
+    private String text1;
+    private String text2;
+
+    public ExampleItem(int img, String txt1, String txt2){
+        imageValue = img;
+        text1 = txt1;
+        text2 = txt2;
+    }
+
+    public int getImageValue(){
+        return imageValue;
+    }
+
+    public String getText1(){
+        return text1;
+    }
+
+    public String getText2(){
+        return text2;
+    }
 }
