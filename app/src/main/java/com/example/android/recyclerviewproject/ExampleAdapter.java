76bfc161aa719fb02327a3912c9dbfc7b47c4d2e
Githel <githelsuico@gmail.com>
2019-05-22 18:35:14 -0700

    Mac edit: finished recyclerview

---
+++ app/src/main/java/com/example/android/recyclerviewproject/ExampleAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.recyclerviewproject;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+
+    private ArrayList<ExampleItem> myListAdapt;
+
+            //configures the contents of a card at initialization
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder   {
+                    public ImageView mImageView;
+                    public TextView mTextView1;
+                    public TextView mTextView2;
+                public ExampleViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.image_view);
+            mTextView1 = itemView.findViewById(R.id.text_view1);
+            mTextView2 = itemView.findViewById(R.id.text_view2);
+        }
+    }
+
+    //Adapter Constructor: configures the info from arraylist into the contents of the card
+    public ExampleAdapter(ArrayList<ExampleItem> list){
+        myListAdapt = list;
+    }
+
+    @NonNull
+    @Override //passes Layout of the card (example_item.xml) to the Adapter
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater obj = LayoutInflater.from(viewGroup.getContext());
+        View newView = obj.inflate(R.layout.example_item, viewGroup, false);
+        ExampleViewHolder mySelf = new ExampleViewHolder(newView);
+        return mySelf;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder exampleViewHolder, int i) {
+        ExampleItem obj =  myListAdapt.get(i);
+        (exampleViewHolder.mImageView).setImageResource(obj.getImageValue());
+        (exampleViewHolder.mTextView2).setText(obj.getText2());
+        (exampleViewHolder.mTextView1).setText(obj.getText1());
+    }
+
+    @Override
+    public int getItemCount() {
+        return myListAdapt.size();
+    }
+}
--- app/src/main/java/com/example/android/recyclerviewproject/MainActivity.java
+++ app/src/main/java/com/example/android/recyclerviewproject/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.android.recyclerviewproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView myRecycler;
+    private RecyclerView.Adapter myAdapter;
+    private RecyclerView.LayoutManager myLayout;
+    private ArrayList<ExampleItem> myList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myList = new ArrayList<>();
+        addItemToList();
+        initRecyclerView();
+    }
+
+    private void addItemToList(){
+        ExampleItem temp = new ExampleItem(R.drawable.ic_imageview, "temp1", "temp2");
+                for(int i = 0; i < 10; i++){
+                    myList.add(temp);
+                }
+    }
+
+    private void initRecyclerView(){
+        //initialize all objects
+        myRecycler = findViewById(R.id.recycler_view);
+        myRecycler.setHasFixedSize(true);
+        myLayout = new LinearLayoutManager(this);
+        myAdapter = new ExampleAdapter(myList);
+
+        //configure objects to the recyclerview
+        myRecycler.setLayoutManager(myLayout);
+        myRecycler.setAdapter(myAdapter);
+
     }
 }
