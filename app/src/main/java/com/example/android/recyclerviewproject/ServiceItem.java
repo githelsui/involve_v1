ddba745fa77b475e99fced98391a1307ccd02be2
GITHELLYN SUICO <800196@district.pylusd.k12.ca.us>
2019-05-24 12:09:02 -0700

    edits to activity_program.xml

--- app/src/main/java/com/example/android/recyclerviewproject/ServiceItem.java
+++ app/src/main/java/com/example/android/recyclerviewproject/ServiceItem.java
@@ -1,7 +1,19 @@
 package com.example.android.recyclerviewproject;
 
-public class ServiceItem {
-    public ServiceItem(){
+import java.util.ArrayList;
 
+public class ServiceItem {
+
+    //class for one service within a program
+
+    private double myHrs;
+    private ArrayList<ServiceItem> serviceList;
+    private String myProgName;
+    private String myDuties; //grab from findViewById(R.id.extra_duties)
+
+    public ServiceItem(double  hours, String prog, String duty){
+        myHrs = hours;
+       myProgName = prog;
+       myDuties = duty;
     }
 }
